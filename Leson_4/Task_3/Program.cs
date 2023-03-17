dce3a4ffef9d18c05051a1db38fa92fefcbab869
Арсений <ars071026@example.com>
2023-03-17 20:46:15 +0500

    Leson_4

---
+++ Leson_4/Task_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая принимает на вход
+//  число N и выдаёт произведение чисел от 1 до N.
+int Multiplication(int num)
+{
+    int result = 1;
+    for(int i = 1 ;i<=num;i++)
+    {
+      result *=i;
+    }
+    return result;
+}
+Console.WriteLine("Ввидите число:");
+int zxc = Multiplication(int.Parse(Console.ReadLine()!));
+Console.WriteLine(zxc);
