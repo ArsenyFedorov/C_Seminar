623dcd8e378093bd04131036209e72b75affa335
Арсений <ars071026@example.com>
2023-03-29 20:41:36 +0500

    afjjf

---
+++ Leson_7/HW/Task_2.1/Program.cs
@@ -0,0 +1,53 @@
+﻿int[,] FilArr(int line, int pillar, int min, int max)
+{
+    int[,] arr = new int[line, pillar];
+    for (int i = 0; i < line; i++)
+    {
+        for (int k = 0; k < pillar; k++)
+        {
+            arr[i, k] = new Random().Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+void Print(int[,] arr)
+{
+    int line = arr.GetLength(0);
+    int pillar = arr.GetLength(1);
+    for (int i = 0; i < line; i++)
+    {
+        for (int k = 0; k < pillar; k++)
+        {
+            Console.Write(arr[i, k]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+void Arr(int[,] arr, int userline, int userpillar)
+{
+    if (arr.GetLength(0) >= userline - 1 && arr.GetLength(1) >= userpillar - 1) Console.WriteLine($"Число с такими индексами в данном массиве: {UserNum( arr, userline, userpillar)}");
+    else Console.WriteLine("В данном массиве нет числа с такими индексами");
+}
+
+int UserNum(int[,] arr, int userline, int userpillar)
+{
+    return arr[userline-1,userpillar-1];
+}
+
+Console.WriteLine("Ввидите количество строк:");
+int line1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввидите количество столбцов:");
+int pillar1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввидите минимальное значениея массива:");
+int min1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввидите максимальное значение массива:");
+int max1 = int.Parse(Console.ReadLine()!);
+int[,] newarr = FilArr(line1, pillar1, min1, max1);
+Print(newarr);
+Console.WriteLine("Ввидите искомую строку:");
+int userline = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввидите искомый столбец:");
+int userpillar = int.Parse(Console.ReadLine()!);
+Arr(newarr, userline, userpillar);
+
---
+++ Leson_7/HW/Task_2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+int[,] FilArr(int line, int pillar, int min, int max)
+{
+    int[,] arr = new int[line, pillar];
+    for (int i = 0; i < line; i++)
+    {
+        for (int k = 0; k < pillar; k++)
+        {
+            arr[i, k] = new Random().Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+void Print(int[,] arr)
+{
+    int line = arr.GetLength(0);
+    int pillar = arr.GetLength(1);
+    for (int i = 0; i < line; i++)
+    {
+        for (int k = 0; k < pillar; k++)
+        {
+            Console.Write(arr[i, k]);
+            Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+}
+string UserNum(int[,] arr, int userline, int userpillar)
+{
+    int line = arr.GetLength(0);
+    int pillar = arr.GetLength(1);
+    for (int i = 0; i < line; i++)
+    {
+        for (int j = 0; j < pillar; j++)
+        {
+            if (i == userline - 1 && j == userpillar-1) return $"Число массива с указанными индексами :{arr[i,j]}";
+        }
+    }
+   return "В данном массиве нет элемента с такими индексами ";
+}
+
+Console.WriteLine("Ввидите количество строк:");
+int line1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввидите количество столбцов:");
+int pillar1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввидите минимальное значениея массива:");
+int min1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввидите максимальное значение массива:");
+int max1 = int.Parse(Console.ReadLine()!);
+int[,] newarr = FilArr(line1, pillar1, min1, max1);
+Print(newarr);
+Console.WriteLine("Ввидите искомую строку:");
+int userline = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввидите искомый столбец:");
+int userpillar = int.Parse(Console.ReadLine()!);
+Console.WriteLine(UserNum(newarr, userline, userpillar));
